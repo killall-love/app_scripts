82f388ede94c8fb560c57ba63b68ede973e81e0b
烟花小神 <1347229508@qq.com>
2022-11-29 20:13:20 +0800

    Update dongdongFarm.js

--- dongdongFarm.js
+++ dongdongFarm.js
@@ -105,7 +105,7 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
 const urlSchema = `openjd://virtual?params=%7B%20%22category%22:%20%22jump%22,%20%22des%22:%20%22m%22,%20%22url%22:%20%22https://h5.m.jd.com/babelDiy/Zeus/3KSjXqQabiTuD1cJ28QskrpWoBKT/index.html%22%20%7D`;
 let lnrun = 0;
 !(async () => {
-    cookiesArr[0] = process.env.JD_APP_COOKIE;
+    cookiesArr[0] = process.env.APP_COOKIE;
     await requireConfig();
     if (!cookiesArr[0]) {
         $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', { "open-url": "https://bean.m.jd.com/bean/signIndex.action" });
