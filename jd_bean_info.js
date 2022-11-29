9b9d96ae0c5519477e5fc65671af681d8b5154d4
烟花小神 <1347229508@qq.com>
2022-11-29 20:13:57 +0800

    Update jd_bean_info.js

--- jd_bean_info.js
+++ jd_bean_info.js
@@ -95,7 +95,7 @@ let myMap = new Map();
 let allBean = 0;
 let cookiesArr = [], cookie = '';
 !(async () => {
-    cookiesArr[0] = process.env.JD_APP_COOKIE
+    cookiesArr[0] = process.env.APP_COOKIE
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
     return;
